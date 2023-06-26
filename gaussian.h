314863070bcbe5b05efd9d225211ece261903c0c
ketch <ketchu15@stud.thga.de>
2023-06-26 15:13:45 +0200

    Füge den entsprechenden Include-Guard hinzu

---
+++ gaussian.h
@@ -0,0 +1,9 @@
+#ifndef GAUSSIAN_H
+#define GAUSSIAN_H
+
+#include <stdio.h>
+
+void print_gaussian_dist(int expected_value, int std_deviation, int amount_of_values, FILE *output_stream);
+
+#endif  // GAUSSIAN_H
+
